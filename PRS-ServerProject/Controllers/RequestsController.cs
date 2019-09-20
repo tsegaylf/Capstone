106bf897fde661d27d5e20637bffc190f567a252
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-20 15:48:59 -0400

    working on the Get request for reviewer method

--- PRS-ServerProject/Controllers/RequestsController.cs
+++ PRS-ServerProject/Controllers/RequestsController.cs
@@ -28,6 +28,14 @@ namespace PRS_ServerProject.Controllers
             return await _context.Request.ToListAsync();
         }
 
+        //private void GetRequestForReviewer(int userId) {
+        //    var request = _context.Request.Find(userId);
+        //    if (request == null) {
+        //        throw new Exception("Instant must not be null");
+        //    }
+        //    request.Status = Where(r => r.Status == "Review" && r.UserId != userId);
+        //}
+
         // GET: api/Requests/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Request>> GetRequest(int id)
